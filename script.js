19806a1f369751b4bcdd2a6a66bf0bc5ea1e8e41
rafaelencinasr <rafaeladalbertoencinas94@gmail.com>
2022-06-20 16:08:31 -0700

    Add validation styling and password check
    Added styling for valid and invalid inputs, created a script that checks
    if the typed in passwords are the same, if they are a green border is set
    to both inputs, if not a red border is applied and a text appears below
    "* Passwords do not match".
    Linked "Create Account" button to the form via the id.
    Added placeholder texts to show examples of correct data input.

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 console.log("Hello world");
+
+let inputPasswordValue;
+let confirmInputPasswordValue;
+
+const passwordInput = document.querySelector('#userPassword');
+
+passwordInput.addEventListener('focusout', ()=>{
+    inputPasswordValue = document.getElementById('userPassword').value;
+    if(inputPasswordValue!=''){
+        checkPassword();
+    }
+    else{
+        confirmPasswordInput.classList.remove("error");
+        passwordInput.classList.remove("error");
+        confirmPasswordInput.classList.remove("correcto");
+        passwordInput.classList.remove("correcto");
+    }
+});
+
+
+
+const confirmPasswordInput = document.querySelector('#userPasswordConfirm');
+
+confirmPasswordInput.addEventListener('focusout', ()=>{
+    confirmInputPasswordValue = document.getElementById('userPasswordConfirm').value;
+    if(confirmInputPasswordValue!=''){
+        checkPassword();
+    }
+    else{
+        confirmPasswordInput.classList.remove("error");
+        passwordInput.classList.remove("error");
+        confirmPasswordInput.classList.remove("correcto");
+        passwordInput.classList.remove("correcto");
+        matchedPasswords.textContent='';
+    }
+});
+
+function checkPassword(){
+    if(inputPasswordValue==confirmInputPasswordValue){
+        console.log("Same password");
+        confirmPasswordInput.classList.remove("error");
+        passwordInput.classList.remove("error");
+
+        confirmPasswordInput.classList.add("correcto");
+        passwordInput.classList.add("correcto");
+        matchedPasswords.textContent='';
+    }
+    else{
+        console.log("Diff passwords");
+        confirmPasswordInput.classList.add("error");
+        passwordInput.classList.add("error");
+
+        confirmPasswordInput.classList.remove("correcto");
+        passwordInput.classList.remove("correcto");
+        matchedPasswords.textContent='* Passwords do not match';
+    }
+};
+
+const matchedPasswords = document.getElementById('matchedPasswords')
+
